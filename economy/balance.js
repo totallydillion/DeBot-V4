abe5bc749748f09aa745ac8f89c7809c9d7ee3cf
Dillion <95979395+totallydillion@users.noreply.github.com>
2023-12-02 14:46:22 -0500

    function-ized getting balance
    Instead of making only the command get the balance, a function now is going to be called when another script, or even the balance script is trying to get the balance. This will save headaches later when pay.js is a thing, and not having to copy and paste code, just to get the balance; we can just use a function. Surprisingly, this little change, costed me 25 minutes of my life I will never get back, all because I forgot a comma, and I do not know why I do programming anymore, programming makes me have too many headaches, sos.

--- economy/balance.js
+++ economy/balance.js
@@ -1,9 +1,11 @@
-econst { SlashCommandBuilder, ChannelType, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, ChannelType, PermissionFlagsBits } = require('discord.js');
 const fs = require('fs');
+
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('balance')
+    .setDMPermission(false)
     .setDescription('View your balance in the Perry Bot!')
     .addUserOption(option => 
         option.setName('user')
@@ -12,13 +14,26 @@ module.exports = {
         async execute(interaction) {
             
             //variables
-            var data = fs.readFileSync(`\\DBot4\\databases\\userSpecific\\balances.json`);
-            var objects = JSON.parse(data);
             const target = interaction.options.getUser('user') || interaction.user;
             let userID = target.id;
+            
+            
+            //get that users balance
+            let balance = await this.getBalance(userID);
+            
+            //display it
+            return interaction.reply({embeds:[{
+                title:`${target.globalName}`,
+                description: `Balance: ${balance.balance} | Bank : ${balance.bank}`
+            }]})
+        },
+        async getBalance(userID){
+            var data = fs.readFileSync(`\\DBot4\\databases\\userSpecific\\balances.json`);
+
+            var objects = JSON.parse(data);
             let specificObject;
-
-
+            
+            
             //for every balance object, find the one with the userID mentioned
             await objects.forEach(objekt => {
                 if(objekt.id == userID)
@@ -27,24 +42,22 @@ module.exports = {
                 }
             });
             
-            //if it is there, display it
+            //if it is there, return that specific balance
             if(specificObject){
-                return interaction.reply({embeds:[{
-                    title:`${target.globalName}`,
-                    description:`Balance: ${specificObject.balance} | Bank: ${specificObject.bank}`
-                }]})
+                return specificObject;
+                
             }
-
+            
             //if there is no data, create a template
             let newData = {
                 "id": userID,
                 "balance": 0,
                 "bank": 0
             }
-
+            
             //push the new data
             objects.push(newData)
-
+            
             //make it JSON-compatible
             let updatedData = JSON.stringify(objects);
             
@@ -54,11 +67,8 @@ module.exports = {
                 
                 //unless there is an error, we should be good
             })
-
-            //display the new users balance
-            return interaction.reply({embeds:[{
-                title:`${target.globalName}`,
-                description: `Balance: ${newData.balance} | Bank : ${newData.bank}`
-            }]})
-        },
-    };
+            
+            //return that users balance
+            return newData;
+        }
+    }
