525d45f2064ef1342fd83f5adc827a112f8180cc
Dillion <95979395+totallydillion@users.noreply.github.com>
2023-12-14 17:17:30 -0500

    embedded gtn

--- games/gtn.js
+++ games/gtn.js
@@ -2,52 +2,73 @@ const { SlashCommandBuilder } = require('discord.js');
 
 var users = [];
 
-//todo: make the game look better with embeds, as well as maybe have one message for all of the guesses, and only send empherial replies, and update one message
-
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('gtn')
     .setDescription('Play a game of Guess The Number!'),
     async execute(interaction) {
         
-        
+        //if the user is already playing , let them know
         if(users.includes(interaction.user.id))
         return interaction.reply("You already have a game that has started!");
         
-        await interaction.reply({content:"A Game Of Guess The Number Has Started! Please guess a number between 1-100", fetchReply: true})
+        //otherwise, start the game
+        await interaction.reply({embeds:[{
+            title:`A Guess The Number Game Has Started!`,
+            description:`${interaction.user}, please guess a number between 1 and 100 to begin!`
+        }], fetchReply: true})
         .then(function(message) {
             
+            //push them into the playing array
             users.push(interaction.user.id);
+
+            //get a random number
             const number = Math.floor(Math.random() * 99) + 1;
             
+            //game properties
             var game = {
                 user: interaction.user.id,
                 turns: 5,
+                guesses: [],
                 num: number
             }
             
+            //create a listener
             const gameFilter = m => m.author.id == interaction.user.id;
-
-            
             const collector = message.channel.createMessageCollector({filter: gameFilter, time: 60_000})
             
+
+            //when the user sends a message
             collector.on('collect', m => {
 
+                //turn it into an int
                 let guess = parseInt(m.content);
 
                 if(guess != game.num){
+                    game.guesses.push(`${guess} (${guess > game.num ? "Lower" : "Higher"})`);
                     game.turns--;
                     
                     if(game.turns != 0){ // wrong answer
-                       interaction.channel.send(`${guess > game.num ? "Lower!":"Higher!"}\n**${game.turns}** Turns Remaining!`)
+                       //interaction.channel.send(`\n**${game.turns}** Turns Remaining!`)
+                       message.edit({embeds:[{
+                            title:`${game.turns} Attempts Remaining!`,
+                            description:`The Number is ${guess > game.num ? "**Lower":"**Higher"} than ${guess}**!\n**__Guesses__**\n${game.guesses.join('\n')}`
+                        }]})
+
                     }
                     else { // game over
-                       interaction.channel.send(`Game over! The number was ${game.num}`);
+                        message.edit({embeds:[{
+                            title:`Game Over!`,
+                            description:`The Winning Number Was: **${game.num}**\n**__Guesses__**\n${game.guesses.join('\n')}`
+                        }]})
                         collector.stop();
                     }
                 }
                 else if(guess == game.num){ //correct
-                    interaction.channel.send("Correct!");
+                    message.edit({embeds:[{
+                        title:`Correct!`,
+                        description:`You have guessed the winning number: **${game.num}** \n**__Guesses__**\n${game.guesses.join("\n")}`
+                    }]})
                     collector.stop();
                 }
             })
